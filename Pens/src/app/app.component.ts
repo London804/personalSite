61d8d4c5d0495b890e8efedd2f3ab74f10b8120b
Alex Chaparro <alexechaparro@gmail.com>
2016-11-08 13:31:49 -0500

    figured out why images were not loading (absolute path is necessary), also got base styles to work

--- Pens/src/app/app.component.ts
+++ Pens/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import {HomeComponent} from './home/home.component'
 import {PortfolioComponent} from './portfolio/portfolio.component'
 
@@ -6,7 +6,8 @@ import {PortfolioComponent} from './portfolio/portfolio.component'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'app works!';
