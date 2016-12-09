b6850059a2a2b6c56c0d313c0c8fbf8e9552660a
Alex Chaparro <alexechaparro@gmail.com>
2016-12-08 23:02:51 -0500

    removing spotify components. Makes more sense to build this as its own angular2 project and then host on the same server. That way it has its own menu and footer

--- Pens/src/app/app.module.ts
+++ Pens/src/app/app.module.ts
@@ -7,16 +7,13 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { HomeComponent } from './home/home.component';
-import { SidebarComponent } from './SpotifyUI/sidebar/sidebar.component';
-import { SearchbarComponent } from './SpotifyUI/searchbar/searchbar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     PortfolioComponent,
-    HomeComponent,
-    SidebarComponent,
-    SearchbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
