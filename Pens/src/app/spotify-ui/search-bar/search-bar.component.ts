b9c408b94505fdbcc7f84d09a15c75a85862d0c9
Alex Chaparro <alexechaparro@gmail.com>
2017-01-23 22:24:04 -0500

    done with searchbar component for now

--- Pens/src/app/spotify-ui/search-bar/search-bar.component.ts
+++ Pens/src/app/spotify-ui/search-bar/search-bar.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent implements OnInit {
+	name: string
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
+  	ngOnInit() {
+  		this.name ="Alex Chaparro"
   }
 
 }
