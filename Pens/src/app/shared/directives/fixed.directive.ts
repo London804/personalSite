1bdc1755643f89ed587496b421675cba1db072fb
Alex Chaparro <alexechaparro@gmail.com>
2017-03-18 21:52:17 -0400

    cant get resize function to fire from directive

--- Pens/src/app/shared/directives/fixed.directive.ts
+++ Pens/src/app/shared/directives/fixed.directive.ts
@@ -1,22 +1,22 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appFixed]'
 })
 export class FixedDirective {
-  constructor() {
-  	console.log('this is working');
-  	// this.onResize(event);
-  }
+	constructor() {
+		console.log('this is working');
+		this.onResize(event);
+	}
+	@HostListener('onResize', ['$event'])
+	  	onResize(event) {
+	 		event.target.innerWidth;
 
- //  	onResize(event) {
- // 		event.target.innerWidth;
-
- // 		if(event.target.innerWidth > '748px') {
-	// 	// this.left = 800 - $(document).scrollLeft()
-	// 	// this.style.left = 800 - document.scrollLeft
-	// 		console.log('you got small');
-	// 	}
-	// }
+	 		if(event.target.innerWidth > '748px') {
+			// this.left = 800 - $(document).scrollLeft()
+			// this.style.left = 800 - document.scrollLeft
+				console.log('you got small');
+			}
+		}
 
 }
--- Pens/src/app/spotify-ui/play-bar/play-bar.component.ts
+++ Pens/src/app/spotify-ui/play-bar/play-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,  NgModule, CUSTOM_ELEMENTS_SCHEMA, HostListener, Input } from '@angular/core';
+import { Component, OnInit,  NgModule, CUSTOM_ELEMENTS_SCHEMA, HostListener, Input, ViewChild } from '@angular/core';
 
 
 @Component({
@@ -12,26 +12,15 @@ export class PlayBarComponent implements OnInit {
   constructor() {
   }
 
-  @HostListener('window:resize', ['$event'])
-
-   onResize(event) {
-     event.target.innerWidth;
-
-     if(event.target.innerWidth < '748px') {
-    // this.left = 800 - $(document).scrollLeft()
-    // this.style.left = 800 - document.scrollLeft
-      console.log('you got small');
-    }
-  }
-
   ngOnInit() {
-    this.onResize(event);
-      // window.innerWidth;
-      // console.log(window.innerWidth);
-
-      // if(window.innerWidth > '748px') {
-      //   // this.left = 800 - $(document).scrollLeft()
-      //   // this.style.left = 800 - document.scrollLeft
-      // }
   }
+
+  // ViewChild(FixedDirective) directive = null
+
+  // ngAfterContentInit() {
+  //   // How can i call functionFromDirective()?
+  //   this.directive.onResize()
+  // }
+
+
 }
